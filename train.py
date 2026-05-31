c269a1a916e86f2e0c901276b87bb84ff1cad770
rgb-hatreya <hatreya05@gmail.com>
2026-05-31 10:51:09 -0700

    alternate- remove import from dataset.py

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import os, glob
 import torch
 import torch.nn as nn
 import pandas as pd
-from torchvision import models
+from torchvision import transforms, models
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 from PIL import Image
-
 from utils import set_seed, get_device
-from dataset import get_dataloaders, get_transforms
 
 DATA_PATH = "/kaggle/input/competitions/ucsc-cse-144-spring-2026-final-project"
 TEST_DIR  = f"{DATA_PATH}/test"
@@ -14,14 +14,19 @@ TEST_DIR  = f"{DATA_PATH}/test"
 set_seed(42)
 device = get_device()
 
-train_loader, _, classes = get_dataloaders(
-    data_dir=DATA_PATH,
-    batch_size=32,
-    val_split=0.0,
-    augment=False,
-    seed=42,
-)
-print("Label mapping sample:", classes[:5])
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+dataset = ImageFolder(f"{DATA_PATH}/train", transform=transform)
+dataset.class_to_idx = {cls: int(cls) for cls in dataset.classes}
+dataset.targets = [int(dataset.classes[t]) for t in dataset.targets]
+dataset.samples = [(s, int(dataset.classes[t])) for s, t in dataset.samples]
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+classes = dataset.classes
+print("Label mapping sample:", list(dataset.class_to_idx.items())[:5])
 
 # Model
 model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
@@ -45,16 +50,15 @@ for epoch in range(10):
         total += labels.size(0)
     print(f"Epoch {epoch+1}: Acc = {correct/total:.3f}")
 
-# Generate submission - all test images, keep .jpg in ID
+# Inference
 model.eval()
-test_transform = get_transforms(augment=False)
 test_images = sorted(glob.glob(TEST_DIR + "/*.jpg"),
-                     key=lambda x: int(os.path.basename(x).replace(".jpg","")))
+                     key=lambda x: int(os.path.basename(x).replace(".jpg", "")))
 ids, preds = [], []
 with torch.no_grad():
     for path in test_images:
         img = Image.open(path).convert("RGB")
-        img = test_transform(img).unsqueeze(0).to(device)
+        img = transform(img).unsqueeze(0).to(device)
         preds.append(model(img).argmax(1).item())
         ids.append(os.path.basename(path))
 
